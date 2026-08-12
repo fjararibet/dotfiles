a70b2af2b65993f31a76346febc1efb8e4ef4295
Felipe Jara <fjararibet@gmail.com>
2026-08-12 12:18:54 -0400

    update

--- hosts/yunco/home.nix
+++ hosts/yunco/home.nix
@@ -1,8 +1,21 @@
 { pkgs, paths, ... }:
 
+let
+  instantclient = pkgs.symlinkJoin {
+    name = "oracle-instantclient-joined";
+    paths = with pkgs.oracle-instantclient; [
+      out
+      lib
+      dev
+    ];
+  };
+in
 {
   imports = [ (paths.home + "/common.nix") ];
   home.packages = with pkgs; [
-    google-cloud-sdk
+    (google-cloud-sdk.withExtraComponents [ google-cloud-sdk.components.cloud-firestore-emulator ])
+    jre
+    oracle-instantclient
   ];
+  home.file."instantclient".source = instantclient;
 }
