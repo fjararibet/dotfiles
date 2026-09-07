9e92b2a3e18ddf09cb6b7c415e2bb4bf9b7cac77
Felipe Jara <fjararibet@gmail.com>
2026-09-07 00:45:03 -0300

    enable t3 code pudu

--- hosts/pudu/configuration.nix
+++ hosts/pudu/configuration.nix
@@ -21,6 +21,8 @@
     curl
   ];
 
+  t3code.enable = true;
+
   # For more information, see `man configuration.nix` 
   # or https://nixos.org/manual/nixos/stable/options#opt-system.stateVersion .
   system.stateVersion = "26.05";
