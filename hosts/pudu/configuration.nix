83b67a4f2387ee5e881516f125d23ac8ab4d9585
Felipe Jara <fjararibet@gmail.com>
2026-09-07 09:36:17 -0300

    Configure Pudu ZFS and disk spindown

--- hosts/pudu/configuration.nix
+++ hosts/pudu/configuration.nix
@@ -9,8 +9,16 @@
   boot.loader.systemd-boot.enable = true;
   boot.loader.efi.canTouchEfiVariables = true;
   boot.supportedFilesystems.zfs = true;
+  boot.zfs.forceImportRoot = false;
 
   boot.zfs.extraPools = [ "zpool" ];
+
+  services.udev.extraRules = ''
+    ACTION=="add|change", SUBSYSTEM=="block", KERNEL=="sd[a-z]", \
+      ATTR{queue/rotational}=="1", \
+      RUN+="${pkgs.hdparm}/bin/hdparm -B 90 -S 241 /dev/%k"
+  '';
+
   networking.hostName = "pudu";
   networking.hostId = "41c929f8";
   networking.networkmanager.enable = true;
